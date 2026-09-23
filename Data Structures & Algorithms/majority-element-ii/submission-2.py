e4439696bda80105c919f71026f368e418a62b5d
Neha <88968867+CodeWithNeha@users.noreply.github.com>
2026-09-23 19:25:18 +0530

    Add: majority-element-ii - submission-2

---
+++ Data Structures & Algorithms/majority-element-ii/submission-2.py
@@ -0,0 +1,18 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        freq = {}
+        res = []
+        n = len(nums)
+        if n == 1:
+            return nums
+        for i in range(0, n):
+            if nums[i] in freq:
+                freq[nums[i]] += 1
+                
+            else:
+                freq[nums[i]] = 1
+            if freq[nums[i]]>n//3 and nums[i] not in res:
+                    res.append(nums[i])
+        return res
+
+        
